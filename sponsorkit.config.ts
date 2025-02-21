f82a3192bd70aa578d0a88611967bac2d25c2863
Xuezhou Dai <daidr@daidr.me>
2025-02-21 13:01:59 +0800

    Update sponsorkit.config.ts

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -31,6 +31,30 @@ export default defineConfig({
     },
   ],
   sponsorsAutoMerge: true,
+  outputDir: '.',
+  formats: ['svg', 'png'],
+  async onSponsorsReady(sponsors) {
+    await fs.writeFile(
+      'sponsors.json',
+      JSON.stringify(
+        sponsors
+          .filter((i) => i.privacyLevel !== 'PRIVATE')
+          .map((i) => {
+            return {
+              name: i.sponsor.name,
+              login: i.sponsor.login,
+              avatar: i.sponsor.avatarUrl,
+              amount: i.monthlyDollars,
+              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
+              org: i.sponsor.type === 'Organization'
+            }
+          })
+          .sort((a, b) => b.amount - a.amount),
+        null,
+        2
+      )
+    )
+  },
   renders: [
     {
       name: 'sponsors',
